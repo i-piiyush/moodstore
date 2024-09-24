2175b22e5d37569e4b08db9879d314a7624cd4f7
Piyush <[email protected]>
2024-09-24 17:37:27 +0530

    added  banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import "./App.css";
+import Banner from "./components/Banner";
 import BottomNav from "./components/BottomNav";
 import Navbar from "./components/Navbar";
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <main className="h-screen w-full overflow-hidden ">
         <Navbar />
+        <Banner />
        <BottomNav />
       </main>
     </>
